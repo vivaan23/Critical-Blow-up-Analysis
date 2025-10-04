981348fe4506883b66b9b8b04945fae880301dd9
vivaan23 <99860000+vivaan23@users.noreply.github.com>
2025-10-05 03:00:08 +0530

    Create plot_growth_curves.m

---
+++ plots/plot_growth_curves.m
@@ -0,0 +1,16 @@
+function plot_growth_curves()
+    figure('Position', [100, 100, 800, 600]);
+    
+    tau = 0:0.1:12;
+    w_d4 = 5 * exp(0.119 * tau);
+    w_d5 = 5 * exp(0.186 * tau);
+    
+    semilogy(tau, w_d4, 'b-', 'LineWidth', 3); hold on;
+    semilogy(tau, w_d5, 'r-', 'LineWidth', 3);
+    xlabel('Logarithmic Time ($\tau$)','Interpreter','latex'); 
+    ylabel('Maximum Amplitude ($w_{max}$)', 'Interpreter','latex');
+    legend('$d$=4, $\gamma$=0.119', '$d$=5, $\gamma$=0.186', 'Location', 'northwest','Interpreter','latex');
+    title('Exponential Growth at Critical Exponent'); 
+    grid on;
+    saveas(gcf, 'growth_curves.png');
+end
