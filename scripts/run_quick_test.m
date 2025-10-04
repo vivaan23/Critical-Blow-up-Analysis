ac776a52df244ff5566c9ea992f793fbc7f61624
vivaan23 <99860000+vivaan23@users.noreply.github.com>
2025-10-05 00:41:01 +0530

    Create run_quick_test.m

---
+++ scripts/run_quick_test.m
@@ -0,0 +1,8 @@
+function run_quick_test()
+    fprintf('=== QUICK VERIFICATION TEST ===\n\n');
+    
+    addpath('src/core', 'src/utils');
+    verify_growth_rates();
+    
+    fprintf('\nCore results verified in < 30 seconds.\n');
+end
