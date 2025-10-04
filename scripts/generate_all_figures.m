d3f3c6c250725030b5692c8030d9133dad9d55f4
vivaan23 <99860000+vivaan23@users.noreply.github.com>
2025-10-05 00:42:25 +0530

    Create generate_all_figures.m

---
+++ scripts/generate_all_figures.m
@@ -0,0 +1,15 @@
+function generate_all_figures()
+    fprintf('Generating all paper figures...\n');
+    
+    addpath('figures');
+    set(0, 'DefaultTextInterpreter', 'none');
+    
+    plot_growth_curves();
+    plot_linear_regression();
+    plot_dimensional_scaling();
+    plot_mae_profiles_simple();
+    plot_convergence();
+    plot_subcritical_convergence();
+    
+    fprintf('All figures saved to results/figures/\n');
+end
